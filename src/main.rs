26a8b02ef404d9a445e461843098a812ee048960
James Daly <jamsiedaly@gmail.com>
2024-02-06 20:07:23 +0100

    Removed redundant macro

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ fn main() -> Result<()> {
             file.read_exact(&mut header)?;
 
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
             // You can use print statements as follows for debugging, they'll be visible when running tests.
